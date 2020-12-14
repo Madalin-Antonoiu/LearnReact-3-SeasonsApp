8994b507c2daec3887b9fd9f8578221e9645c751
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-14 11:19:32 +0000

    Getting Geolocation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 const Application = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
   return (
     <div>
       <div> Hi there!</div>
