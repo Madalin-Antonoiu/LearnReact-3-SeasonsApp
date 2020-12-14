2d30051790fda2ef97a359b79031e53d9882fb71
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-14 12:15:54 +0000

    Treating errors with respect

--- src/index.js
+++ src/index.js
@@ -9,20 +9,28 @@ class Application extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         // We called setState!
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render
   render() {
-    return <div>Latitude : {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude : {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage || "None"}
+      </div>
+    );
   }
 }
 
