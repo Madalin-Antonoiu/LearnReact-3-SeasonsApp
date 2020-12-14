d2a21a46775b4f6d8c9748a0bca778d68b0b553d
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-14 10:53:25 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Application = () => {
+  return <div> Hi there!</div>;
+};
+
+ReactDOM.render(<Application />, document.querySelector("#root"));
