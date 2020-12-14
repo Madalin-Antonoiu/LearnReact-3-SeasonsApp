473361bb2503bc6b806e46433688956de563d17c
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-14 11:39:21 +0000

    Converted to Class-based Component!

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import SeasonDisplay from "./SeasonDisplay";
+//import SeasonDisplay from "./SeasonDisplay";
 
-const Application = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+//Functional component
+// const Application = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
 
-  return (
-    <div>
-      <div> Hi there!</div>
-      <SeasonDisplay />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div> Hi there!</div>
+//       <SeasonDisplay />
+//     </div>
+//   );
+// };
+
+//Class-based component
+class Application extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Latitude : </div>;
+  }
+}
 
 ReactDOM.render(<Application />, document.querySelector("#root"));
